0e9814eaed8fbd35b87b05629b48863801f6d38b
Kayke Cabral <125477411+KaykeCabral@users.noreply.github.com>
2024-05-15 09:17:50 -0300

    s

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function calculaTempo(tempoObjetivo){
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0]);
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
     for (let i = 0; i < contadores.length; i++) {
        //contadores[i].textContent = calculaTempo(tempos[i]);
     }
